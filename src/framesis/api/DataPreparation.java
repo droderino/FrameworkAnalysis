3af9236746021d01e456ede7fca77cab4226c495
droderino <abraun.dev@googlemail.com>
2014-03-21 15:54:33 +0100

    updated Interface

--- src/framesis/api/DataPreparation.java
+++ src/framesis/api/DataPreparation.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public interface DataPreparation {
 
-	public static final String XMLFILE = "xmlFile";
+	public static final String FILE = "file";
 	public static final String PREPARATEDFILE = "convertedFile";
 	public static final String ISSUEROOT = "root";
 	public String prepare(Map<String, String> params);
